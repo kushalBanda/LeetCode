7f0a42931264bb59d66846b784af64a5e0162571
Kushal Banda <kushalbanda265@gmail.com>
2026-01-06 22:46:45 +0530

    Add validPalindrome function to check if a string is a palindrome, including sample usage and output for test cases.

---
+++ LeetCode/125. Valid Palindrome/validPalindrome.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution: 
+    def isPalindrome(self, s: str) -> bool:
+        s_lower = s.lower()
+        s_final = [i for i in s_lower if i.isalnum()]
+        print(s_final)
+
+        return s_final == s_final[::-1]
+
+
+solution = Solution()
+print(solution.isPalindrome("A man, a plan, a canal: Panama"))
+print(solution.isPalindrome("race a car"))

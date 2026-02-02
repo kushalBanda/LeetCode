7cd174849c7a0267a736256ffb278d572a0b039a
Kushal Banda <kushalbanda265@gmail.com>
2026-02-02 22:30:51 +0530

    Add maxProfit.py implementation and maxProfit.png asset for LeetCode problem 121, enhancing algorithm documentation and resources.

---
+++ LeetCode/121. Best Time to Buy and Sell Stock/maxProfit.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+
+        return max_profit
+
+solution = Solution()
+print(solution.maxProfit([7,1,5,3,6,4]))

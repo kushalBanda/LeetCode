5b0ead93ca7b7e83c9a80a6eeb63338829f78ee9
Kushal Banda <kushalbanda265@gmail.com>
2026-02-15 14:26:28 +0530

    Remove debug print statement from isPalindrome method in validPalindrome.py to clean up the code.

--- LeetCode/125. Valid Palindrome/validPalindrome.py
+++ LeetCode/125. Valid Palindrome/validPalindrome.py
@@ -3,8 +3,6 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s_lower = s.lower()
         s_final = [i for i in s_lower if i.isalnum()]
-        print(s_final)
-
         return s_final == s_final[::-1]
 
 
